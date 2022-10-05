3d49d2ab1beb97b4989a77d39761524bd4e3337d
jimo33 <wrokstuff@gmail.com>
2022-10-04 21:49:47 -0500

    Updated File

--- home.py
+++ home.py
@@ -1,12 +1,14 @@
 from selenium import webdriver
+# from selenium.webdriver.common.devtools.v105.dom().selenium
 
+# from import.selenium.webdriver
 import misc
 
 PATH = "C:\Automation\driver\chromedriver.exe"
 
 driver = webdriver.Chrome(PATH)
 driver.get(misc.url)
-
+# driver.find_element(by.ID)
 # driver.manage().window().maximize()
 
 
